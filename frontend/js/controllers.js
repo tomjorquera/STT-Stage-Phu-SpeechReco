a01b8ba71eb27235995acc8d5db97c4758f17e85
Mignastor <frank@Franks-MBP.gso.lan>
2016-01-21 15:35:53 +0100

    test node java os name

--- frontend/js/controllers.js
+++ frontend/js/controllers.js
@@ -15,6 +15,17 @@ angular.module('myApp.controllers', []).
     error(function(data, status, headers, config) {
       $scope.name = 'Error!';
     });
+
+    $http({
+      method: 'GET',
+      url: '/testnodeJava/os'
+    }).
+    success(function(data, status, headers, config) {
+      $scope.osName = data.osJava;
+    }).
+    error(function(data, status, headers, config) {
+      $scope.osName = 'Error!';
+    });
   }).
   controller('MyCtrl1', function($scope) {
     // write Ctrl here
